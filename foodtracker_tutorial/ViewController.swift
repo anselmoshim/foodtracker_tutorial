31d42330480f7eba64df83c2d756ee899a3940c9
Anselmo Shim <anselmoshim@gmail.com>
2017-05-20 21:35:50 -0500

    Initial Commit

---
+++ foodtracker_tutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  foodtracker_tutorial
+//
+//  Created by Anselmo Shim on 5/20/17.
+//  Copyright © 2017 Anselmo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
